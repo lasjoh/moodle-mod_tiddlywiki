00f25bb0ab3b54310ee7f51e48fde9df2885baa2
lasjoh <53155181+lasjoh@users.noreply.github.com>
2023-06-01 18:58:19 +0200

    Update version.php

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2023042400;       // The current module version (Date: YYYYMMDDXX).
-$plugin->requires  = 2023041800;    // Requires this Moodle version.
+$plugin->requires  = 2020041800;    // Requires this Moodle version.
 $plugin->component = 'mod_tiddlywiki'; // Full name of the plugin (used for diagnostics)
 $plugin->cron      = 0;
